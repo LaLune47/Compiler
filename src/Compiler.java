66677c1ac0780364cecf6e182039717eec618fa9
qmy <qmy_buaa@buaa.edu.cn>
2022-10-02 03:00:49 +0800

    词法分析输出到指定文件

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,13 +1,17 @@
 import component.Token;
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.List;
 
 public class Compiler {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         LexParser lexParser = new LexParser(Config.inputFilePath);
         List<Token> tokenList = lexParser.parse();
         for (Token token: tokenList) {
-            System.out.println(token.toString());
+            // System.out.println(token.toString());
+            PrintStream ps = new PrintStream(Config.outputFilePath);
+            System.setOut(ps);
         }
     }
 }
