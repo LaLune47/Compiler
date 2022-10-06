2de2fb083d8074a4b15b51664758e5655847a2a9
qmy <qmy_buaa@buaa.edu.cn>
2022-10-06 21:25:36 +0800

    嗯嗯补两个文件

---
+++ src/AstBuilder.java
@@ -0,0 +1,13 @@
+import AST.Node;
+import component.Token;
+import java.util.List;
+
+public class AstBuilder {
+    
+    public AstBuilder(){}
+    
+    public Node CompUnit(List<Token> tokenList, Integer index) {
+        // todo  语法分析主体
+    }
+    
+}
---
+++ src/AstPrinter.java
@@ -0,0 +1,14 @@
+package AST;
+
+public class AstPrinter {
+    private Node ast;
+    
+    public AstPrinter(Node ast) {
+        this.ast = ast;
+    }
+    
+    public void PrintSyntaxParser() {
+        // todo  怎么对树结构进行一些打印啊啊啊
+    }
+    
+}
